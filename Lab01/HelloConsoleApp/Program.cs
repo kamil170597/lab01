c4576c8bf1bf1a6264ec125a7e7ec0cbe372ba3c
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-03-17 17:47:32 +0100

    Lab01
    Pierwszy do samodzielnego zadania

---
+++ Lab01/HelloConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powitanie.");
+            Console.Write("Podaj imie:");
+            string imie;
+            imie = Console.ReadLine();
+            Console.Write("Podaj nazwisko:");
+            string nazwisko;
+            nazwisko = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie} {nazwisko}");
+            Console.WriteLine($"Witaj {imie} {nazwisko}. Czy Pan {nazwisko} rzeczywiscie nazywa sie {imie}");
+
+            Console.Write("Podaj Wiek:");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+            if(wiek < 67)
+            {
+                Console.WriteLine("do emerytury zostalo ci {0}",67-wiek );
+            }
+            else
+            {
+                Console.WriteLine(" jestes emerytem");
+            }
+        }
+    }
+}
