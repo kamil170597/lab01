053a73655a06ac7641ef7cf2cbe8aab4868b4427
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-03-25 14:38:29 +0100

    Update Program.cs

--- Lab01/HelloConsoleBatch/Program.cs
+++ Lab01/HelloConsoleBatch/Program.cs
@@ -11,53 +11,55 @@ namespace HelloConsoleBatch
         static void Main(string[] args)
         {
 
-            string imie;
-            string nazwisko;
-            int wiek = Convert.ToInt32(args[2]);
+            string imie="";
+            string nazwisko="";
+            int wiek=0;
 
             //Console.WriteLine($"Witaj {imie} {nazwisko}");
 
             //Console.WriteLine($"Twoj wiek: {wiek}");
 
-        if(args.Length != 0)
-        {
-           if(args.Length ==1)
-           {
-                imie = args[0];
-                Console.WriteLine($"Witaj {imie}");
-
-           }else
-                if(args.Length == 2)
+            if(args.Length != 0)
+            {
+            wiek = Convert.ToInt32(args[2]);
+                if(args.Length ==1)
                 {
                     imie = args[0];
-                    nazwisko = args[1];
-                    Console.WriteLine($"Witaj {imie} {nazwisko}");
+                        Console.WriteLine($"Witaj {imie}");
 
                 }else
-                    if(wiek>0)
-            {       
+                    if(args.Length == 2)
+                    {
+                        imie = args[0];
+                        nazwisko = args[1];
+                        Console.WriteLine($"Witaj {imie} {nazwisko}");
+
+                    }else
+                if(wiek>0)
+                {       
                    
-                    imie = args[0];
-                    nazwisko = args[1];
+                imie = args[0];
+                nazwisko = args[1];
+
                     if(wiek>=67)
-                {
-                    Console.WriteLine("Jestes emerytem");
+                    {
+                        Console.WriteLine("Jestes emerytem");
 
-                }
-                else
-                {
-                    Console.WriteLine($"Do emerytury zostalo ci: {67-wiek}");
-                }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Do emerytury zostalo ci: {67-wiek}");
+                    }
 
-            }else
-                 {
-                    Console.WriteLine("Bledne Dane");
-                 }
+                }else
+                    {
+                        Console.WriteLine("Bledne Dane");
+                    }
            
-        }else
-            {
-                Console.WriteLine("Bledne Dane");
-            }
+            }else
+                {
+                    Console.WriteLine("Bledne Dane");
+                }
         }    
     }
 
