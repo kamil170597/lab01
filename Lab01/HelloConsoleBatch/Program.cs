e40dc8a180c48211bed595369e4e38dd8e3569dc
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-03-25 13:34:06 +0100

    Plik wsadowy

---
+++ Lab01/HelloConsoleBatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string imie = args[0];
+            string nazwisko = args[1];
+            int wiek = Convert.ToInt32(args[2]);
+
+            Console.WriteLine($"Witaj {imie} {nazwisko}");
+
+            Console.WriteLine($"Twoj wiek: {wiek}");
+
+            if(wiek>0)
+            {
+                if(wiek>=67)
+                {
+                    Console.WriteLine("Jestes emerytem");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Do emerytury zostalo ci: {67-wiek}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bledne Dane");
+            }
+
+
+
+        }
+    }
+}
